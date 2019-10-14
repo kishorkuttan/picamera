059da33665724fb2ac749f13514ccf6f9531ffb2
kishorkuttan <48623612+kishorkuttan@users.noreply.github.com>
2019-10-14 16:06:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python3
+
 from importlib import import_module
 import os
 from flask import Flask, render_template, Response
-# check if root
-if os.getuid() != 0:
-    print("I can't run as a mortal. Sorry. Try to run me with admin privileges")
-    exit()
+
 # import camera driver
 
 Camera = import_module('camera_opencv').Camera
@@ -37,4 +34,4 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, threaded=True)
+    app.run(host='0.0.0.0', debug=True)
